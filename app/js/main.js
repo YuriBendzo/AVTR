6765047ba5142cef6a8e31aee60be9f8eff44f5b
xaim-dev <yurabendzo20@gmail.com>
2022-02-28 11:17:31 +0200

    finished desktop version

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,12 @@
 $(function(){
 
-    $('.your-class').slick({
-    })
+    $('.design__slider').slick({
+        arrows: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        variableWidth: true,
+        prevArrow: '<img class="arrow arrow-left" src="images/Arrow-left.svg"></img>',
+        nextArrow: '<img class="arrow arrow-right" src="images/Arrow-right.svg"></img>'
+    });
 });
+
