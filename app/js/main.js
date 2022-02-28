5b8e63c5188867d514d53b9935d333e4326d3b6f
xaim-dev <yurabendzo20@gmail.com>
2022-02-28 21:57:56 +0200

    finished adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -6,7 +6,16 @@ $(function(){
         slidesToScroll: 1,
         variableWidth: true,
         prevArrow: '<img class="arrow arrow-left" src="images/Arrow-left.svg"></img>',
-        nextArrow: '<img class="arrow arrow-right" src="images/Arrow-right.svg"></img>'
+        nextArrow: '<img class="arrow arrow-right" src="images/Arrow-right.svg"></img>',
+        responsive: [
+            {
+              breakpoint: 501,
+              settings: {
+                variableWidth: false,
+                slidesToShow: 1,
+              }
+            }
+          ]
     });
 });
 
